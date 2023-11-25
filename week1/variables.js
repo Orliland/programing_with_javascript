d5e6185c34223347fdfaa1b444d76f82b0f66201
Orlando Verdin <git@orli.land>
2023-11-24 22:09:41 -0700

    Take variable lesson

---
+++ week1/variables.js
@@ -0,0 +1,19 @@
+"John";
+"Hello, John";
+
+var person;
+var person = "John";
+
+person;
+
+console.log("Hello", person);
+
+var greeting = "Hello";
+
+console.log(greeting, person);
+
+greeting = "Hola";
+
+person = "Juan";
+
+console.log(greeting, person);

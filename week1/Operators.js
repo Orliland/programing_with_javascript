f08a7e9857053b073968d4dba913b1a5bf72ffd8
Orlando Verdin <git@orli.land>
2023-11-26 22:11:45 -0700

    Lesson Operators

---
+++ week1/Operators.js
@@ -0,0 +1,21 @@
+/* Arithmetic Operators in JS
+  + Addition
+  - Subtraction
+  * Multiplication
+  / Division
+*/
+
+/* Comparison Operators in JS:
+  > Greater than
+  < Less than
+  == Equal to
+*/
+
+console.log(2 + 2);
+console.log(1 + 2 + 3 + 4 + 5);
+console.log(20 - 10);
+console.log(10 * 10);
+console.log(100 / 10);
+console.log(3 > 2);
+console.log(3 < 2);
+console.log(10 == 10);

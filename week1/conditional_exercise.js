c104ff6c622c7e7fe2aa55b272bf779173e0d56e
Orlando Verdin <git@orli.land>
2023-11-26 23:40:35 -0700

    Conditional exercise task 1

---
+++ week1/conditional_exercise.js
@@ -0,0 +1,12 @@
+// task 1
+
+var age = 23;
+if (age >= 65) {
+  console.log("You get your income from your pension");
+} else if (age < 65 && age >= 18) {
+  console.log("Each month you get your salary");
+} else if (age < 18) {
+  console.log("You get an allowance");
+} else {
+  console.log("The value of the age variable is not numerical");
+}

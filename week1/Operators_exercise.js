f75a64526790d5c0fcecb57b4a06bfd3f4f2ee40
Orlando Verdin <git@orli.land>
2023-11-26 22:21:05 -0700

    Operator excersice task 2

--- week1/Operators_exercise.js
+++ week1/Operators_exercise.js
@@ -1,3 +1,9 @@
 // Task 1
 var score = 8;
 console.log("Mid-level skills:", score > 0 && score < 10);
+
+// Task 2
+var timeRemaining = 0;
+var energy = 10;
+
+console.log("Game over:", timeRemaining === 0 || energy === 0);

cc188499839c08419b2764091bfda3c3e467b77a
Orlando Verdin <git@orli.land>
2023-11-26 23:42:15 -0700

    Conditional exercise task 2

--- week1/conditional_exercise.js
+++ week1/conditional_exercise.js
@@ -10,3 +10,31 @@ if (age >= 65) {
 } else {
   console.log("The value of the age variable is not numerical");
 }
+
+// task 2
+var day = "Sunday";
+switch (day) {
+  case "Monday":
+    console.log("Today is Monday");
+    break;
+  case "Tuesday":
+    console.log("Today is Tuesday");
+    break;
+  case "Wednesday":
+    console.log("Today is Wednesday");
+    break;
+  case "Thursday":
+    console.log("Today is Thursday");
+    break;
+  case "Friday":
+    console.log("Today is Friday");
+    break;
+  case "Saturday":
+    console.log("Today is Saturday");
+    break;
+  case "Sunday":
+    console.log("Today is Sunday");
+    break;
+  default:
+    console.log("The value of the day variable is not a day of the week");
+}

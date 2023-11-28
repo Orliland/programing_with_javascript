7766d829526a5db993b538644c390406b9e93668
Orlando Verdin <git@orli.land>
2023-11-27 23:13:02 -0700

    Conditional and loops exercise task 2

--- week1/conditional_and_loops.js
+++ week1/conditional_and_loops.js
@@ -10,3 +10,20 @@ for (var i = 1; i <= 10; i++) {
     console.log(i);
   }
 }
+
+// task 2
+for (var i = 1; i <= 10; i++) {
+  switch (i) {
+    case 1:
+      console.log("Gold medal");
+      break;
+    case 2:
+      console.log("Silver medal");
+      break;
+    case 3:
+      console.log("Bronze medal");
+      break;
+    default:
+      console.log(i);
+  }
+}

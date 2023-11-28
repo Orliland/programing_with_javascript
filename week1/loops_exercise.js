d120473e7d08234ff4a48e42348eaa0393b02985
Orlando Verdin <git@orli.land>
2023-11-27 22:18:44 -0700

    Loops exercise task 2

--- week1/loops_exercise.js
+++ week1/loops_exercise.js
@@ -3,3 +3,9 @@ for (var i = 1; i <= 5; i++) {
   console.log(i);
 }
 console.log("Counting completed!");
+
+// task 2
+for (var i = 5; i >= 1; i--) {
+  console.log(i);
+}
+console.log("Countdown finished!");

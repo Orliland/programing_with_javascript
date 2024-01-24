3e77ec9219fa6e9de07153b8f586372c8cca29b0
Orlando Verdin <git@orli.land>
2024-01-23 23:32:19 -0700

    Exercise Arrays and Objects

---
+++ week2/ArraysObjects_exercise.js
@@ -0,0 +1,18 @@
+var clothes = [];
+clothes.push("jacket");
+clothes.push("t-shirt");
+clothes.push("trousers");
+clothes.push("shoes");
+clothes.push("socks");
+
+clothes.pop();
+
+clothes.push("hat");
+
+console.log(clothes);
+
+var favCar = {};
+favCar.color = "red";
+favCar.covertible = false;
+
+console.log(favCar);
